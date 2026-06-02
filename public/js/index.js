05e9945391d74c16c4f38eabe18d8dade6e755e3
billdingwall <dingwall.wr@gmail.com>
2026-06-02 04:34:54 -0600

    refactor: remove legacy HTML files and unused partials while updating base layouts

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-// conditionally load livereload & GA based on host
+// conditionally load GA based on host
 
 var hostName = window.location.hostname;
 
@@ -9,28 +9,16 @@ if (hostName === 'williamdingwall.com') {
   gtag('config', 'G-KH99855ZFW');
 }
 
-if (hostName === 'localhost') {
-  var livereloadScript = document.createElement('script');
-  livereloadScript.setAttribute('src','http://localhost:35729/livereload.js');
-  document.head.appendChild(livereloadScript);
-}
-
 window.onload = function(){
 
-  var splideCheck = u('.splide').length;
+  var splides = document.querySelectorAll('.splide');
   
-  if(splideCheck > 0) {
-
-    console.log('TEST')
+  if (splides.length > 0) {
 
     var splide = new Splide( '.splide', {
       type    : 'loop',
       autoplay: 'play',
       interval: 10000
-    } );
-
-    splide.on( 'autoplay:playing', function ( rate ) {
-    console.log( rate ); // 0-1
     } );
 
     splide.mount();
